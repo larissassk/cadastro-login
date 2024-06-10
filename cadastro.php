4a6a1a5bd6992a03fd23c9ff6511eff1cd006db2
larissassk <larissasantos200702@gmail.com>
2024-06-10 18:11:07 -0300

    atualiza css

--- cadastro.php
+++ cadastro.php
@@ -69,9 +69,9 @@
       <div>
         <h3 class="float-md-start mb-0"><strong>TechWeb</strong></h3>
         <nav class="nav nav-masthead justify-content-center float-md-end">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
           <a class="nav-link" href="login.php">Login</a>
-          <a class="nav-link" href="cadastro.php">Cadastre-se</a>
+          <a class="nav-link active" href="cadastro.php">Cadastre-se</a>
         </nav>
       </div>
     </header>
--- login.php
+++ login.php
@@ -72,7 +72,7 @@
         <h3 class="float-md-start mb-0"><strong>TechWeb</strong></h3>
         <nav class="nav nav-masthead justify-content-center float-md-end">
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-          <a class="nav-link" href="login.php">Login</a>
+          <a class="nav-link active" href="login.php">Login</a>
           <a class="nav-link" href="cadastro.php">Cadastre-se</a>
         </nav>
       </div>
